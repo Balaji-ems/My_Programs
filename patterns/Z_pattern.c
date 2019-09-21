9ac9baa51bce28a72b9802985cfc6c272e9f5f48
Balaji Eswaramoorthi <42486678+BalajiEswaramoorthi@users.noreply.github.com>
2019-09-21 09:24:38 +0530

    Create Z_pattern.c

---
+++ patterns/Z_pattern.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ *
+ *  *****
+ *     *
+ *    *
+ *   *
+ *  *****
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+
+{
+	int input, row, coloumn, test;
+
+	scanf("%d", &input);
+	for (row = 0; row < input; row++) {
+		for (coloumn = 0; coloumn < input; coloumn++) {
+
+			test = coloumn + row == input - 1;
+			if (row == 0 || row == input-1 || test)
+				printf("*");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+}
