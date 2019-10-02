e534d3a057795f5e56010ef5357217b094f6c6b5
Balaji Eswaramoorthi <balajisubasheckp@yahoo.com>
2019-10-02 14:13:35 +0530

    New programs

---
+++ C_Basics/Max_element_in_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int sizeof_array, index, max_element;
+
+A:
+	printf("Enter a positive element greater than 1: ");
+	scanf("%d", &sizeof_array);
+
+	if (sizeof_array < 2)
+		goto A;
+
+	int *ptr = (int *) malloc (sizeof_array * sizeof(int));
+
+	printf("\nEnter the elements to find the maximum\n");
+
+	scanf("%d", ptr);
+	max_element = *ptr;
+
+	for(index = 1;index < sizeof_array; index++) {
+		scanf("%d", ptr + index);
+		if (*(ptr + index) > max_element)
+			max_element = *(ptr + index);
+	}
+
+	printf("The maximum element is %d", max_element);
+	return 0;
+}
---
+++ C_Basics/Reverse_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int array_size, index, *ptr, left, right;
+
+	printf("Enter the array size : ");
+	scanf("%d", &array_size);
+
+	ptr = (int *)malloc(array_size * sizeof(int));
+
+	printf("Enter the array elements : ");
+	for(index = 0; index < array_size; index++)
+		scanf("%d", ptr+index);
+	
+	left = 0;
+	right = array_size - 1;
+	
+	while (left < right) {
+		*(ptr + left) = (*(ptr + left) + *(ptr + right)) - (*(ptr + right) = *(ptr + left));
+		left++;
+		right--;
+	}
+	
+	printf("The reversed array is: ");
+	for(index = 0; index < array_size; index++)
+		printf("%d ",*(ptr+index));
+		
+}
